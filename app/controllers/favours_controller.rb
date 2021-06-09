6180c9787f192dae5d69fb8ca6eb815383671955
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-06-09 21:00:59 +0530

    favours new html

---
+++ app/controllers/favours_controller.rb
@@ -0,0 +1,27 @@
+class FavoursController < ApplicationController
+
+
+
+
+
+
+
+
+
+
+  def create
+  @favour = Favour.new(favour_params)
+  @favour.user = current_user
+    if @favour.save
+      redirect_to favours_path, notice: 'Your favour was added!'
+    else
+      render :new
+    end
+  end
+
+
+
+
+  
+
+end
