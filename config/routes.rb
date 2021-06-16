b585dab046c930fa4b438b0cdb7c3aa23ac688b0
Marilou Pinera <mapinera9@gmail.com>
2021-06-16 15:19:29 +0200

    linked to chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :favours, only: [:index, :show, :create, :new] do
-    resources :applications, only: [:create, :edit, :update], shallow: true do
+    resources :applications, only: [:create, :edit, :update, :show], shallow: true do
       resources :reviews, only: [:new, :create, :index]
       member do
       get 'mark_as_done'
       end
     end
    end
-  resources :applications, only: [:index, :show] do
+  resources :applications, only: [:index] do
     resources :messages, only: [:create]
   end
   resources :credits, only: [:index, :show]
