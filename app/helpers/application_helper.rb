3d1e8454d4f96951e19cfffaedb5088bd7e6256e
Marilou Pinera <mapinera9@gmail.com>
2021-06-17 11:50:30 +0200

    avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
-  def user_avatar(user, size=40)
+  def user_avatar(user, options = { height: 40, width: 40 })
     if user.avatar.attached?
-      user.avatar.variant(resize: "#{size}x#{size}!")
+      cl_image_tag user.avatar.key, options
+    else
+      image_tag "useravatar.png", options
     end
   end
 end
