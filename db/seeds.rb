2c3bfc3602ca8f7f6e0083da19d962dd3b64ebde
Nirmala Luwang <nirmalaluwang@gmail.com>
2021-06-18 12:58:29 +0530

    seeds with marilou

--- db/seeds.rb
+++ db/seeds.rb
@@ -44,14 +44,18 @@ peter.save
 # p peter.errors.full_messages
 
 
-
 p john = User.new(name: 'John', address: '1091 GR Amsterdam', email: 'john@gmail.com', skills: [housekeeping, gardening])
-
 john.password = 'qwerty'
 file = URI.open('https://images.pexels.com/photos/3754208/pexels-photo-3754208.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
 john.avatar.attach(io: file, filename: 'John.jpg', content_type: 'image/jpg')
 
 john.save
+
+p marilou = User.new(name: 'Marilou', address: '1091 GR Amsterdam', email: 'marilou@gmail.com', skills: [housekeeping, gardening])
+marilou.password = 'qwerty'
+file = URI.open('https://avatars.githubusercontent.com/u/82805747?v=4')
+marilou.avatar.attach(io: file, filename: 'marilou.jpg', content_type: 'image/jpg')
+marilou.save
 
 
 p richard = User.new(name: 'Richard', address: '1012 PH Amsterdam', email: 'richard@gmail.com', skills: [accounting, clerical])
@@ -60,7 +64,6 @@ file = URI.open('https://images.pexels.com/photos/5485840/pexels-photo-5485840.j
 richard.avatar.attach(io: file, filename: 'Richard.jpg', content_type: 'image/jpg')
 
 richard.save
-
 
 p nancy = User.new(name: 'Nancy', address: '1016 GV Amsterdam', email: 'nancy@gmail.com', skills: [accounting, clerical, gardening])
 file = URI.open('https://images.pexels.com/photos/2726111/pexels-photo-2726111.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260')
@@ -79,16 +82,16 @@ sally.save
 # Favour Types
 
 
-p plumbing = FavourType.find_or_create_by(content: 'plumbing', amount_available: 20)
+p plumbing = FavourType.find_or_create_by(content: 'Plumbing', amount_available: 20)
 plumbing.save
 
-p gardening = FavourType.find_or_create_by(content: 'gardening', amount_available: 25)
+p gardening = FavourType.find_or_create_by(content: 'Gardening', amount_available: 25)
 gardening.save
 
-p financial = FavourType.find_or_create_by(content: 'financial', amount_available: 30)
+p financial = FavourType.find_or_create_by(content: 'Financial', amount_available: 30)
 financial.save
 
-p clerical = FavourType.find_or_create_by(content: 'clerical', amount_available: 30)
+p clerical = FavourType.find_or_create_by(content: 'Clerical', amount_available: 30)
 clerical.save
 
 
@@ -97,7 +100,7 @@ clerical.save
 
 
 
-p plumbing_house = Favour.find_or_create_by(duration: 'one day', description: 'plumbing all my pipes', zone: 'Central', user: john, favour_type: plumbing)
+p plumbing_house = Favour.find_or_create_by(duration: '2 hours', description: 'Plumbing all my pipes', zone: '10 RH Amsterdam', user: john, favour_type: plumbing)
 file = URI.open('https://www.jgbathrooms.com.au/wp-content/uploads/2020/08/Elanoragibson13.jpg')
 plumbing_house.photo.attach(io: file, filename: 'bathroom.jpg', content_type: 'image/jpg')
 plumbing_house.save
@@ -155,6 +158,12 @@ application_5.reviews.create(rating: 4, content: 'Excellent work, really reommen
 application_5.save
 p application_5.errors.full_messages
 
+p application_6 = Application.find_or_create_by(content: 'Would be great to perform this task. Lots of relevant experience.', status: 'accepted', done: 'completed', favour: gardening_house, user: marilou)
+application_6.reviews.create(rating: 4, content: 'Good job, really reommend.')
+application_6.reviews.create(rating: 4, content: 'Excellent work, really reommend.')
+application_6.reviews.create(rating: 4, content: 'Excellent work, really reommend.')
+application_6.save
+p application_6.errors.full_messages
 
 # Credits
 
@@ -171,6 +180,8 @@ credit_3.save
 p credit_4 = Credit.find_or_create_by(amount_redeemed: 0, user: nancy)
 credit_4.save
 
+p credit_5 = Credit.find_or_create_by(amount_redeemed: 0, user: sally)
+credit_5.save
 
-
-
+p credit_6 = Credit.find_or_create_by(amount_redeemed: 0, user: marilou)
+credit_6.save
